0873ab482e3d882c0bf5d12f2fc4eadbd1ce770d
WiktorKuch <wiktor.kucharek1997@gmail.com>
2023-07-02 17:59:06 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,14 +12,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
     
 
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 
 
 app.MapControllers();
 
 app.Run();
+
